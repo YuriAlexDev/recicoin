643cdbdb98b2830b14a77a01c10fe3677d58a270
leonamlucius <leonam253@gmail.com>
2024-05-14 15:22:23 -0300

    Fecth

---
+++ JavaScript/mapa.js
@@ -0,0 +1,29 @@
+const content = document.querySelector(".content");
+
+
+
+const url = "http://localhost:8080/login"
+
+async function getName(){
+
+    const reponse =  await fetch(url);
+
+    console.log(reponse);
+
+    const data = await reponse.json();
+    
+    console.log(data);
+
+
+    data.map((post) =>{
+
+
+        content.innerHTML = post.name
+
+
+
+    })
+
+}
+
+getName();
